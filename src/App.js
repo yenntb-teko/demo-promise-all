cc65ef99712cd60a8b924894d7fcd2ecd72e29b4
yenntb-teko <yen.ntb@teko.vn>
2019-11-27 15:54:39 +0700

    call api product list

--- src/App.js
+++ src/App.js
@@ -1,100 +1,98 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { getProducts, getProduct } from "./service";
+import { getProducts } from "./service";
 
 function App() {
   // demo promise
-  const timeOutResolve = t => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(`Completed in ${t}ms`);
-      }, t);
-    });
-  };
+  // const timeOutResolve = t => {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve(`Completed in ${t}ms`);
+  //     }, t);
+  //   });
+  // };
 
-  const createArray = () => {
+  // const createArray = () => {
+  //   let array = [];
+  //   for (let i = 0; i < 50; i += 1) {
+  //     array.push(100);
+  //   }
+  //   return array;
+  // };
+
+  // const callPromise = async () => {
+  //   const startTime = Date.now();
+  //   const array = createArray();
+  //   for (let i = 0; i < array.length; i += 1) {
+  //     await timeOutResolve(array[i]);
+  //   }
+  //   const endTime = Date.now();
+  //   alert(endTime - startTime);
+  // };
+
+  // const promiseAll = async array => {
+  //   for (let i = 0; i < array.length; i += 100) {
+  //     const subArray = array.slice(i, i + 100);
+  //     let promises = [];
+  //     subArray.map(item => promises.push(timeOutResolve(item)));
+  //     await Promise.all(promises).then(() => {
+  //       promises = [];
+  //     });
+  //   }
+  //   return Date.now();
+  // };
+
+  // const callPromiseAll = async () => {
+  //   const startTime = Date.now();
+  //   const array = createArray();
+  //   const endTime = await promiseAll(array);
+  //   alert(endTime - startTime);
+  // };
+
+  const numberPage = 100;
+  const batch10 = 10;
+
+  const createNumberPages = () => {
+    let page = 1;
     let array = [];
-    for (let i = 0; i < 50; i += 1) {
-      array.push(100);
+    for (let i = 0; i < numberPage; i += 1) {
+      array.push(page);
+      page += 1;
     }
     return array;
   };
 
-  const callPromise = async () => {
+  const getProductOneByOne = async () => {
+    const pages = createNumberPages(); // 100 page, from 1 to 100
+    console.log("pages: ", pages);
+
     const startTime = Date.now();
-    const array = createArray();
-    for (let i = 0; i < array.length; i += 1) {
-      await timeOutResolve(array[i]);
+    for (let i = 0; i < pages.length; i += 1) {
+      await getProducts(pages[i]);
     }
     const endTime = Date.now();
     alert(endTime - startTime);
   };
 
-  const promiseAll = async array => {
-    for (let i = 0; i < array.length; i += 100) {
-      const subArray = array.slice(i, i + 100);
+  const getProductsByBatch = async batch => {
+    const pages = createNumberPages();
+
+    const startTime = Date.now();
+    for (let i = 0; i < pages.length; i += batch) {
+      const pageBatch = pages.slice(i, i + batch);
       let promises = [];
-      subArray.map(item => promises.push(timeOutResolve(item)));
-      await Promise.all(promises).then(() => {
-        promises = [];
-      });
+      pageBatch.map(page => promises.push(getProducts(page)));
+      await Promise.all(promises.map(p => p.catch(e => e)));
     }
-    return Date.now();
+    const endTime = Date.now();
+    return alert(endTime - startTime);
   };
 
-  const callPromiseAll = async () => {
-    const startTime = Date.now();
-    const array = createArray();
-    const endTime = await promiseAll(array);
-    alert(endTime - startTime);
-  };
-
-  const createSkuList = n => {
-    const skus = [
-      "191100707",
-      "191100706",
-      "191100705",
-      "191100701",
-      "191100702",
-      "191100699",
-      "1202938",
-      "191100697",
-      "191100695",
-      "191100756"
-    ];
-    let array = [];
-    for (let i = 0; i < n; i++) {
-      array = [...array, ...skus];
-    }
-    return array;
-  };
-
-  const getAllProduct = () => {
-    const listSku = createSkuList(100);
-    const startTime = Date.now();
-    getProducts(listSku).then(response => {
-      if (response) {
-        const endTime = Date.now();
-        alert(endTime - startTime);
-      }
-    });
-  };
-
-  const createSkuBatchs = () => {
-    const skuBatchs = [];
-    const skuList = createSkuList(100);
-    for (let i = 0; i < skuList.length; i += 10) {
-      let skus = skuList.slice(i, i + 10);
-      skuBatchs.push(skus);
-    }
-    return skuBatchs;
-  };
-
-  const getProductsByBatch = () => {
+  const getAllProductsOneTime = () => {
     let promises = [];
-    const skuBatchs = createSkuBatchs();
-    skuBatchs.map(skus => promises.push(getProducts(skus)));
+    const pages = createNumberPages();
+    pages.map(page => promises.push(getProducts(page)));
 
     const startTime = Date.now();
     Promise.all(promises.map(p => p.catch(e => e))).then(res => {
@@ -103,15 +101,15 @@ function App() {
     });
   };
 
-  const getProductOneByOne = async () => {
-    const skuBatchs = createSkuBatchs();
-    const startTime = Date.now();
-    for (let i = 0; i < skuBatchs.length; i++) {
-      await getProducts(skuBatchs[i]);
-    }
-    const endTime = Date.now();
-    return alert(endTime - startTime);
-  };
+  // const createSkuBatchs = () => {
+  //   const skuBatchs = [];
+  //   const skuList = createNumberPages(numberPage);
+  //   for (let i = 0; i < skuList.length; i += 10) {
+  //     let skus = skuList.slice(i, i + 10);
+  //     skuBatchs.push(skus);
+  //   }
+  //   return skuBatchs;
+  // };
 
   // initial code
   return (
@@ -130,14 +128,14 @@ function App() {
           Learn React
         </a> */}
         <div>
-          <button type="button" onClick={getAllProduct}>
-            call 1 api
-          </button>
-          <button type="button" onClick={getProductOneByOne}>
+          <button type="button" onClick={() => getProductOneByOne()}>
             get product one by one
           </button>
-          <button type="button" onClick={getProductsByBatch}>
-            get products by batch
+          <button type="button" onClick={() => getProductsByBatch(batch10)}>
+            get products by batch 10
+          </button>
+          <button type="button" onClick={() => getAllProductsOneTime()}>
+            get all products one time
           </button>
         </div>
       </header>
--- src/service.js
+++ src/service.js
@@ -1,9 +1,12 @@
-export function getProducts(skus) {
+export function getProducts(page) {
   const requestOptions = {
-    method: "GET"
+    method: "GET",
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjExLCJ0eXBlIjoidXNlciIsImVtYWlsIjoieWVuLm50YkB0ZWtvLnZuIiwiaWF0IjoxNTc0ODIyNzk5LCJleHAiOjE1NzQ5MDkxOTl9.dXOBljWFVnJTsoEIJQL4RVkaPX0DdF1xv7cb_Ez9GSw`
+    }
   };
   return fetch(
-    `https://listing-dev.services.teko.vn/api/search/?channel=pv_online&terminal=phongvu&skus=${skus.join()}`,
+    `https://catalog-dev.phongvu.vn/products/?pageSize=10&page=${page}`,
     requestOptions
   ).then(
     response => {
@@ -16,20 +19,20 @@ export function getProducts(skus) {
   );
 }
 
-export function getProduct(sku) {
-  const requestOptions = {
-    method: "GET"
-  };
-  return fetch(
-    `https://listing-dev.services.teko.vn/api/search/?channel=pv_online&terminal=phongvu&skus=${sku}`,
-    requestOptions
-  ).then(
-    response => {
-      if (!response.ok) {
-        return Promise.reject(response.statusText);
-      }
-      return response.json();
-    },
-    error => Promise.reject(error)
-  );
-}
+// export function getProduct(sku) {
+//   const requestOptions = {
+//     method: "GET"
+//   };
+//   return fetch(
+//     `https://listing-dev.services.teko.vn/api/search/?channel=pv_online&terminal=phongvu&skus=${sku}`,
+//     requestOptions
+//   ).then(
+//     response => {
+//       if (!response.ok) {
+//         return Promise.reject(response.statusText);
+//       }
+//       return response.json();
+//     },
+//     error => Promise.reject(error)
+//   );
+// }
